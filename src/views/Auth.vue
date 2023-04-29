06f934936e64d9c0e93fcf9201354391edd00507
haziqimac <hpprojects00@gmail.com>
2023-04-29 18:55:19 +0800

    Originv2

---
+++ src/views/Auth.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <v-container>
+            <v-row
+            align="center">
+                <v-col align-self="center">
+                    <h1>Login</h1>
+                </v-col>
+            </v-row>
+
+            <form action="">
+                <v-text-field placeholder="Email Address" outlined />
+                <v-text-field placeholder="Password" outlined />
+            </form>
+            <v-btn block color="primary">LOGIN</v-btn>
+        </v-container>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
