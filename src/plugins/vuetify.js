b8a933034fbc2cc356b6664ba595d888edc3fcd2
haziqimac <hpprojects00@gmail.com>
2023-05-08 15:39:34 +0800

    enabling dark theme for the web app

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,5 +3,15 @@ import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
+
 export default new Vuetify({
-});
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: '#2196f3',
+          background: '#121212',
+        },
+      },
+    },
+  })
