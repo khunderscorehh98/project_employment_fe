2adb92318ee1a919747732aeea4df090a7996f0b
haziqimac <hpprojects00@gmail.com>
2023-05-08 15:36:44 +0800

    created contact form component

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <v-text-field
+        placeholder="Email Address"
+        outlined
+        ></v-text-field>
+        <v-text-field
+        placeholder="Name"
+        outlined
+        ></v-text-field>
+        <v-text-field
+        placeholder="Phone Number"
+        outlined
+        ></v-text-field>
+        <v-text-field
+        placeholder="Title"
+        outlined
+        ></v-text-field>
+        <v-textarea
+        outlined
+        placeholder="Enter your message here.."></v-textarea>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
