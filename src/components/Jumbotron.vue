d863146c4f5a52422a6bb2cf8e7ba58b93189b51
haziqimac <hpprojects00@gmail.com>
2023-05-08 15:38:22 +0800

    wip on jumbotron

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -4,15 +4,23 @@
       <v-container fill-height>
         <v-layout align-center>
           <v-flex>
-            <h3 class="display-3">Welcome to the site</h3>
-
-            <span class="subheading"
-              >Lorem ipsum dolor sit amet, pri veniam forensibus id. Vis
-              maluisset molestiae id, ad semper lobortis cum. At impetus
-              detraxit incorrupte usu, repudiare assueverit ex eum, ne nam
-              essent vocent admodum.</span
-            >
-
+            <v-row>
+              <v-col block>
+                <h3 class="display-3">Welcome to EmployMe</h3>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <h4>Get hired with EmployMe</h4>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <span class="subheading">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur eum omnis voluptatem sint quaerat iste iusto rem itaque vel animi, voluptatum soluta aliquam numquam architecto deleniti facere quas dolor natus?
+                </span>
+              </v-col>
+            </v-row>
             <v-divider class="my-3"></v-divider>
 
             <div class="title mb-3">Check out our newest features!</div>
