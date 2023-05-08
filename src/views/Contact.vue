4b595bf6858a9cf11890eb785cabcda9f43dc34c
haziqimac <hpprojects00@gmail.com>
2023-05-08 15:41:00 +0800

    contact page created

---
+++ src/views/Contact.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="contactPad">
+            <v-card>
+                <div class="formPad">
+                    <Contact />
+
+                </div>
+            </v-card>
+        </div>
+    </div>
+</template>
+
+<script>
+import Contact from '../components/ContactForm.vue'
+    export default {
+        components: {
+            Contact
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.contactPad{
+padding-left: 30%;
+padding-right: 30%;
+padding-top: 3%;
+padding-bottom: 10%;
+}
+.formPad{
+    padding: 5%;
+}
+</style>
