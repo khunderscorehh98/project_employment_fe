a7de01f6bb956f716ca62414627a34928b742070
haziqimac <hpprojects00@gmail.com>
2023-05-09 11:00:35 +0800

    create profile page

--- src/views/CreateProfile.vue
+++ src/views/CreateProfile.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
-
+        <v-container>
+            <v-card class="cardPad">
+                <CreateForm />
+            </v-card>
+        </v-container>
     </div>
 </template>
 
 <script>
+import CreateForm from '../components/CreateProfile.vue'
     export default {
+        components: {
+            CreateForm
+        },
         data() {
             return {
 
@@ -18,5 +26,8 @@
 </script>
 
 <style lang="scss" scoped>
-
+.cardPad{
+    padding: 3%;
+    margin: 10%;
+}
 </style>
